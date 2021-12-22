3b1719f0fada5812e27fd2fb2e76bf09f2d53fdf
omar-alrihily <omar.8260@gmail.com>
2021-12-22 09:30:28 +0300

    finsh git and post

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,39 @@ const app=express()
 const db=require('./db');
 const Todo=require('./todo');
 
+app.use(express.json())
+
 
 // console.log(Todo);
 app.get('/',(req,res)=>{
     res.json(' GeT / is Working')
 })
+
+app.get('/tasks',(req,res)=>{
+    Todo.find({},(err,data)=>{
+        if (err){
+            console.log("Error",err);
+
+        } else {
+            res.json(data)
+        }
+    })
+});
+
+app.post('/tasks',(req,res)=>{
+
+    console.log("25:",req.body);
+    Todo.create(req.body,(err,newTask)=>{
+        if (err){
+            console.log("Error",err);
+
+        } else {
+            res.status(201).json(newTask);
+        }
+    })
+});
+
+
 
 
 app.listen(5000,()=>{
